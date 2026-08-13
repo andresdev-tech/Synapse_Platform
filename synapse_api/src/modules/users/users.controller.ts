e4d2e7b049246e82a55f7b7dc0d79b545e14fc20
andresdev-tech <andrewjaramillo315@gmail.com>
2026-08-13 15:17:25 -0500

    modificacion en el modulo usuario, ya que desde la vista administrador genereba un error al ver todos los usuarios registrados by andres

--- synapse_api/src/modules/users/users.controller.ts
+++ synapse_api/src/modules/users/users.controller.ts
@@ -81,7 +81,9 @@ export class UsersController {
         res: Response
     ) {
         try {
-            const user = await UsersService.update(Number(req.params.id), req.body);
+            const id = (req as any).user?.id;
+            console.log("userId: ", id)
+            const user = await UsersService.update(id, req.body);
             res.status(200).json({
                 success: true,
                 data: user
